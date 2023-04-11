bd71983dd5c4188c1449eba85e1fc0ca39bea0ce
Ismael Lopez Hernandez <ismael.lopez.hernandes@gmail.com>
2023-04-11 09:49:17 -0500

    logMONGOURI

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 let mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
-
+console.log(process.env.MONGO_URI)
 let Person;
 
 const createAndSavePerson = (done) => {
